2b2e4378f3ced1a5045fb585f0853643f14ce1ab
42419 <3493950250@qq.com>
2025-11-23 14:46:52 +0800

    chore: add release keystore and configure signing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,11 +29,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = "upload"
+            keyPassword = "123456"
+            storeFile = file("upload-keystore.jks")
+            storePassword = "123456"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
